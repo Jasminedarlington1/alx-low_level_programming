07e48955e6144ecfc21d84dc9eacf649f4bf2483
Jasmine <jasmineenams@gmail.com>
2023-02-16 15:35:12 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry level
  *
- * Result: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	result 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
