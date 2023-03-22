86cd8133ba63ff898ca89e815fb79a90caff0eb9
Jasmine <jasmineenams@gmail.com>
2023-03-22 10:04:53 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error"\n);
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	a = 0;
+
+	while (a % b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+}
