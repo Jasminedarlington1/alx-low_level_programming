7346952524dbf9c010728c8b4eb81e94f221fb79
Jasmine <jasmineenams@gmail.com>
2023-04-03 10:31:58 -0700

    print all elements of listint_t

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,15 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num;
+	size_t i = 0;
 
 	while (h)
-		printf("%d\n", h->n);
-	
-	num = 0;
-	num++;
+	{
+	i++;
 
 	h = h->next;
-
-	return (num);
+	}
+	return (i);
 }
