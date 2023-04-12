93e2953574f4af2fd06de6621ca1968f0be9bf42
Jasmine <jasmineenams@gmail.com>
2023-04-12 08:32:51 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (a = 0; text_content[a];)
-			len++;
+			a++;
 	}
 	k = open(filename, O_WRONLY | O_APPEND);
 	w = write(k, text_content, a);
