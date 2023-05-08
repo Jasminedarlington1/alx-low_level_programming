3c4653ecd9362b1b5a8b37251a08eeae8318de89
Jasmine <jasmineenams@gmail.com>
2023-05-08 09:18:59 -0700

    creating files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,22 +11,20 @@ int create_file(const char *filename, char *text_content)
 	int fd, x;
 	size_t y = 0;
 	ssize_t r = 0, w;
-	char *t;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1)
 		return (-1);
-	text_content = t;
-	if (t != NULL)
+	if (text_content != NULL)
 	{
-		for (x = 0; t[x] != '\0'; x++)
+		for (x = 0; text_content[x] != '\0'; x++)
 		{
 			y += 1;
 			r += 1;
 		}
-		w = write(fd, t, y);
+		w = write(fd, text_content, y);
 		if (w == -1 || r != w)
 			return (-1);
 	}
