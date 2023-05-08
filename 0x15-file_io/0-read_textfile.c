e77aa4d282615f96456e2813d8a6bcdd22083f7a
Jasmine <jasmineenams@gmail.com>
2023-05-08 08:32:03 -0700

    readable text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,26 +5,34 @@
  * POSIX standard output
  * @filename: name of file
  * @letters: number of letters
- * Return: 0
+ * Return: number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t r, w;
-	char b;
+	ssize_t r, p, x;
+	char *buff;
+	int fd;
+	size_t y = 0;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDWR);
 	if (fd == 1)
 		return (0);
 
-	b = malloc(sizeof(char) * letters);
-	r = read(fd, b, letters);
-	w = write(STDOUT_FILENO, b, r);
+	buff = malloc(sizeof(char) * letters);
+	r = read(fd, buff, letters);
+	if (r == -1)
+		return (0);
 
+	for (x = 0; x < r; x++)
+		y += 1;
+	p = write(STDOUT_FILENO, buff, r);
+	if (p == -1 || p != r)
+		return (0);
 	close(fd);
-	free(b);
+	free(buff);
 
-	return (0);
+	return (r);
 }
